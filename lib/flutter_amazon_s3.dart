ca25c04129868391b67cc15e99a1d7a775331f90
kaashyapan <sunder.narayanaswamy@gmail.com>
2019-02-17 22:11:43 +0530

    change region

---
+++ lib/flutter_amazon_s3.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class FlutterAmazonS3 {
+  static const MethodChannel _channel =
+      const MethodChannel('flutter_amazon_s3');
+
+  static Future<String> uploadImage(
+      String filepath, String bucket, String identity) async {
+    final Map<String, dynamic> params = <String, dynamic>{
+      'filePath': filepath,
+      'bucket': bucket,
+      'identity': identity,
+    };
+    final String imagePath =
+        await _channel.invokeMethod('uploadImageToAmazon', params);
+    return imagePath;
+  }
+}
